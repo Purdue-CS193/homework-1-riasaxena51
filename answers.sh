1f28d782e3014bf9b62f751f4cdd81f485da4377
Ria Saxena <saxena89@data.cs.purdue.edu>
2022-09-16 23:15:12 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -1,45 +1,47 @@
-#!/usr/bin/env bash
-
+#!/usr/bin/env bash 
 # Changes to the ORIGIN directory
 cd ORIGIN
 
-# Task 1 
+# Task 1
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 
 # Task 2
+rm Memes/SpongeBob/money.jpeg
 
-
-# Task 3
-
+# Task 
+mv Memes/Star\ Wars/fafsa.jpg Memes/SpongeBob
 
 # Task 4
-
+rm -r Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv -i  Programs/Age.cpp Programs/C++
 
 # Task 7
-
+cp -r  Programs/Java Homework/CS180/Project_1
 
 # Task 8
-
+mv Programs/Count.c Programs/Goodbye.c -t Programs/C
 
 # Task 9
-
+mv Programs/Name.py Programs/Space.py -t Programs/"Python 3"
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls ..
 
 # Task 12
-: '
+
 Type your vim/nano customizations below:
+:set number
+:let g:onedark_termcolors=265
+:set shiftwidth=2 stoptabstop=2 tabstop=2
 
 
 
-'
